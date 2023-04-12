32aa29a39570e7ccd40d131d2cedfcb2a23fdde6
Douglas A <andreani.dr@gmail.com>
2023-04-12 13:06:44 -0300

    vercel: add vercel configuration and build steps

---
+++ api/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from domain.movie_view_model import MoviesViewModel
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "movies"
+
+
+@app.route("/api/movies")
+def movies():
+    m = MoviesViewModel()
+    return m.get_movies_use_case(1)
+
+
+@app.route("/api/movie")
+def movie():
+    m = MoviesViewModel()
+    movie_id = request.args.get('id')
+    return m.get_movie_use_case(movie_id)
